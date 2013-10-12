8b0f7712e403c3c04d735dcacf5e6703bddcc1ef
ywng <ngyikwai@ngyiks-iMac.local>
2013-10-11 20:23:44 -0700

    build a hellow world

---
+++ TaxiBook/TaxiBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TaxiBook
+//
+//  Created by ngyik wai on 10/11/13.
+//  Copyright (c) 2013 ngyik wai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "driverAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([driverAppDelegate class]));
+    }
+}
---
+++ TaxiBook/TaxiBookTests/TaxiBookTests.m
@@ -0,0 +1,34 @@
+//
+//  TaxiBookTests.m
+//  TaxiBookTests
+//
+//  Created by ngyik wai on 10/11/13.
+//  Copyright (c) 2013 ngyik wai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TaxiBookTests : XCTestCase
+
+@end
+
+@implementation TaxiBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
