058572b69337f7f6ec2392d377ebbef636a25797
Jason Ng <ywng@ust.hk>
2014-02-14 15:29:33 +0800

    .change file struct

---
+++ TaxiBook/TaxiBook/ViewControllers/CustomNavigationControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomNavigationControllerViewController.h
+//  TaxiBook
+//
+//  Created by Yik Wai Ng Jason on 14/2/14.
+//  Copyright (c) 2014 taxibook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomNavigationControllerViewController : UINavigationController
+
+@end
